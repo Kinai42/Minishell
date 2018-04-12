55a4d3e6cd302f1d612d8a47507d8c09f6d888f6
Damien BAUDUIN <dbauduin@e1r2p6.42.fr>
2018-04-12 20:30:50 +0200

    merci

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dbauduin <dbauduin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 18:19:22 by dbauduin          #+#    #+#             */
-/*   Updated: 2018/04/08 05:11:30 by dbauduin         ###   ########.fr       */
+/*   Updated: 2018/04/12 20:29:00 by dbauduin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,27 @@ int	main(int ac, char **av, char **env)
 {
 
 	char	**envcpy;
-
+	char	buf[2];
+	char	l[2048];
 	envcpy = ft_tabcpy(env);
 
 	int i = 0;
 	(void)ac;
 	if (av)
-	i = 0;
+		i = 0;
 	while(envcpy[i])
 		printf("%s\n", envcpy[i++]);
-	chdir("/Users/damien");
-	printf("user=Damien\n");
+	i = 0;
+	while(42)
+	{
+		read(0, buf, 1);
+		l[i] = buf[0];
+		if (l[i] == '\n')
+		{
+			printf("%s", l);
+			l[i] = 0;
+		}
+		i++;
+	}
 	return (0);
 }
